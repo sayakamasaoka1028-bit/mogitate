239b75c17b4dd434a8ddab9b4e242dbadabe16e8
sayaka <sayaka.masaoka1028@gmail.com>
2026-02-11 09:00:05 +0900

    add products list page (route, controller, view)

---
+++ src/resources/views/products/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>商品一覧</title>
+</head>
+<body>
+    <h1>商品一覧</h1>
+
+    <ul>
+        <li>りんご</li>
+        <li>みかん</li>
+        <li>バナナ</li>
+    </ul>
+</body>
+</html>
+
